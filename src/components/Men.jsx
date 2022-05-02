91e5241c847eae553a20d56f5f1d7fad62c29777
rahulhuli26 <rahulhulikoppe38@gmail.com>
2022-05-02 17:32:32 +0530

    Updated changes

--- src/components/Men.jsx
+++ src/components/Men.jsx
@@ -18,7 +18,7 @@ export const Men = () => {
         <h2>Mens</h2>
         <Grid data-testid="products-container">
         {filterData.map((e) => (
-         <ProductCard id ={e.id}  title={e.title} price = {e.price} category={e.category} image = {e.image}/>
+         <ProductCard id ={e.id} brand={e.brand}  title={e.title} price = {e.price} category={e.category} image = {e.image}/>
         ))}
       </Grid>
       </>
